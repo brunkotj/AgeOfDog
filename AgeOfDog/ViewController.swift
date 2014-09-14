60f73c2e46725f5e31000575af9f84a63ee0df68
Tom Brunkow <brunkotj@gmail.com>
2014-09-14 12:51:16 -0500

    Initial Commit

---
+++ AgeOfDog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AgeOfDog
+//
+//  Created by Thomas on 9/14/14.
+//  Copyright (c) 2014 Tom Brunkow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
