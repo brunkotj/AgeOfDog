310a05a9c62b00aa9d4d242a031635d93ad469a0
Tom Brunkow <brunkotj@gmail.com>
2014-09-15 09:16:21 -0500

    First Commit for age of laika challenge

--- AgeOfDog/ViewController.swift
+++ AgeOfDog/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    
+    @IBOutlet weak var convertedDogYearsLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+    
+        let stringFromTextField = humanYearsTextField.text
+        let optionalIntFromTextField = stringFromTextField.toInt()
+        let intFromOptional = optionalIntFromTextField!
+        convertedDogYearsLabel.hidden = false
+        convertedDogYearsLabel.text = "\(intFromOptional * 7)" + " human years"
+        humanYearsTextField.resignFirstResponder()
+        
+    }
+    
+    @IBAction func convertToScientificDogYears(sender: UIButton) {
+        let humanYearsDouble = Double((humanYearsTextField.text as NSString).doubleValue)
+        var dogConversionConstant:Double
+        if humanYearsDouble < 2 {
+            dogConversionConstant = humanYearsDouble * 10.5
+        } else {
+            dogConversionConstant = ((humanYearsDouble - 2) * 4) + 21
+        }
+        convertedDogYearsLabel.hidden = false
+        convertedDogYearsLabel.text = "dog years = \(dogConversionConstant)"
+        humanYearsTextField.resignFirstResponder()
+        
+    }
+    
 
 }
 
